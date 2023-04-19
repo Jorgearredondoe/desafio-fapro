157b1da704c47d76529582150154ad53b31287db
Jorgearredondoe <jorge.arredondo.e@gmail.com>
2023-04-19 17:22:46 -0400

    chore(UF-table): migration

--- UFTracker/UFTrack/models.py
+++ UFTracker/UFTrack/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class UF(models.Model):
+    value = models.FloatField()
+    date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.date} {self.value}'
