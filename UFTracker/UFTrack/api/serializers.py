73414e534d56f8476882c558dbe023854abf1056
Jorgearredondoe <jorge.arredondo.e@gmail.com>
2023-04-21 14:51:24 -0400

    refactor(): format and comments

--- UFTracker/UFTrack/api/serializers.py
+++ UFTracker/UFTrack/api/serializers.py
@@ -21,16 +21,20 @@ class UFSerializer(serializers.ModelSerializer):
 
 class UFDetailSerializer(serializers.ModelSerializer):
     value = serializers.SerializerMethodField()
+    # Uncomment this line if you want to get the date of the UF
     # date = serializers.SerializerMethodField()
 
     class Meta:
         model = UF
-        exclude = ['id', 'created_at', 'updated_at', 'date']
+        exclude = ['id', 'created_at', 'updated_at']
+        # Comment this line if you want to get the date of the UF
+        exclude.append('date')
 
     def get_value(self, instance):
         if instance.value == -1.0:
             return None
         return instance.value
 
+    # Uncomment this line if you want to get the date of the UF
     # def get_date(self, instance):
     #     return instance.date.strftime('%d-%m-%Y')
